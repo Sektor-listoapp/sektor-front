74526b01a83a00c168c5ffe6b036b3e0f690d866
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2024-12-30 22:02:52 -0400

    tst

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import ConfigProvider from "antd/lib/config-provider";
 import es from "antd/locale/es_ES";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
