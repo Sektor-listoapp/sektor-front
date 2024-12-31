6f621e63e8e27b16290608552b0c17bdd22b8471
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2024-12-30 22:20:31 -0400

    test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,34 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        "3xl": "1920px",
+      },
+      fontFamily: {
+        "arial-rounded": [
+          "Arial Rounded MT Bold",
+          "Arial Rounded MT",
+          "sans-serif",
+        ],
+        "century-gothic": ["Century Gothic", "sans-serif"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          50: "#F0F9FF",
+          100: "#E0F2FE",
+          200: "#B7D9E2",
+          300: "#7DD3FC",
+          400: "#1F5372",
+          500: "#182F48",
+          600: "#162A42",
+          700: "#0D1C2C",
+          800: "#09161E",
+          900: "#060F13",
+        },
+      },
+      background: {
+        "primary-gradient":
+          "linear-gradient(250deg,rgba(255, 255, 255, 0.5) -20%,rgba(183, 217, 226, 0.7) -6%,#182f48 60%)",
       },
     },
   },
