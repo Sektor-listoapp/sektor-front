8f14159a3840d6aac108073db1173e5db82f3295
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2024-12-30 22:01:28 -0400

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import ConfigProvider from "antd/lib/config-provider";
+import es from "antd/locale/es_ES";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ConfigProvider locale={es}>
+      <Component {...pageProps} />;
+    </ConfigProvider>
+  );
 }
