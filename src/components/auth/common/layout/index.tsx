1947d8108ce1c20fc511719a58cfeddd7ad75c75
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2025-01-02 17:05:48 -0400

    test

--- src/components/auth/common/layout/index.tsx
+++ src/components/auth/common/layout/index.tsx
@@ -24,7 +24,7 @@ const AuthLayout = ({
 
   return (
     <div className="min-h-svh secondary-gradient w-full flex flex-col justify-between items-start gap-8 lg:block lg:relative lg:overflow-hidden">
-      <FullScreenLoader className={isLoading ? "opacity-100" : "opacity-0"} />
+      {isLoading && <FullScreenLoader />}
       <Header />
       <div className="hidden lg:block absolute bg-blue-500 right-[50%] inset-y-0 -top-[20%] w-[125vw] h-[125vw] rounded-full xl:right-2/4 xl:-top-[25%] 2xl:w-[100vw] 2xl:h-[100vw] 2xl:-top-[50%] " />
 
