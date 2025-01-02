42f4063463f75c39b456638633ecdb003108a027
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2025-01-02 17:03:45 -0400

    test

--- src/components/auth/common/layout/index.tsx
+++ src/components/auth/common/layout/index.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "../header";
 import { useAuthStore } from "@/store/auth";
 import { useRouter } from "next/router";
+import FullScreenLoader from "@/components/ui/full-screen-loader";
 import { ROUTES } from "@/constants/router";
 
 const AuthLayout = ({
@@ -9,17 +10,21 @@ const AuthLayout = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  const { push, isReady } = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+  const { replace, isReady } = useRouter();
   const isAuthenticated = useAuthStore((state) => state.getIsAuthenticated)();
 
   useEffect(() => {
     if (isReady && isAuthenticated) {
-      push(ROUTES.HOME);
+      replace(ROUTES.HOME);
+      return;
     }
-  }, [isAuthenticated, isReady, push]);
+    setIsLoading(false);
+  }, [isAuthenticated, isReady, replace]);
 
   return (
     <div className="min-h-svh secondary-gradient w-full flex flex-col justify-between items-start gap-8 lg:block lg:relative lg:overflow-hidden">
+      <FullScreenLoader className={isLoading ? "opacity-100" : "opacity-0"} />
       <Header />
       <div className="hidden lg:block absolute bg-blue-500 right-[50%] inset-y-0 -top-[20%] w-[125vw] h-[125vw] rounded-full xl:right-2/4 xl:-top-[25%] 2xl:w-[100vw] 2xl:h-[100vw] 2xl:-top-[50%] " />
 
