9c13f37192a20fc5679c2ffb9c738f9699aefa3e
Angel Rey <angelrey.dev@gmail.com>
2025-03-21 23:16:47 -0400

    update URL social media link encoding

--- src/components/shared/organization-details-modal/organization-social-media-links/index.tsx
+++ src/components/shared/organization-details-modal/organization-social-media-links/index.tsx
@@ -1,4 +1,7 @@
-import { SocialMediaLinkType } from "@/lib/sektor-api/__generated__/types";
+import {
+  SocialMediaLinkType,
+  SocialMediaPlatform,
+} from "@/lib/sektor-api/__generated__/types";
 import { cn } from "@/utils/class-name";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
@@ -8,6 +11,8 @@ interface OrganizationSocialMediaLinksProps
   extends React.HTMLAttributes<HTMLDivElement> {
   socialMediaLinks: SocialMediaLinkType[];
 }
+
+const { EmergencyPhone, Phone, Whatsapp } = SocialMediaPlatform;
 
 const OrganizationSocialMediaLinks = ({
   socialMediaLinks,
@@ -25,15 +30,23 @@ const OrganizationSocialMediaLinks = ({
       {socialMediaLinks.map((link, index) => {
         const { platform, url } = link;
         const icon = SOCIAL_MEDIA_LINK_ICONS[platform];
+
+        const isPhoneNumber = [EmergencyPhone, Phone, Whatsapp].includes(
+          platform
+        );
+        const normalizedUrl = url.startsWith("http") ? url : `https://${url}`;
+
         return (
-          <button
-            className="w-fit flex items-center justify-start gap-3"
+          <a
+            className="w-fit flex items-center justify-start gap-3 text-blue-500"
             key={`${platform}-${index}`}
-            onClick={() => window.open(url, "_blank")}
+            href={isPhoneNumber ? `tel:${url}` : normalizedUrl}
+            target="_blank"
+            rel="noreferrer noopener"
           >
             <FontAwesomeIcon icon={icon} />
             <span className="underline">{platform}</span>
-          </button>
+          </a>
         );
       })}
     </section>
