efab97484d0c2b94efe9d2a49818bfb2f6e5e33e
Angel Rey <angelrey.dev@gmail.com>
2025-03-21 21:33:11 -0400

    update validation for images

--- src/components/ui/upload-input/index.tsx
+++ src/components/ui/upload-input/index.tsx
@@ -13,6 +13,7 @@ interface UploadInputProps {
   imageUrl: string;
   onImageChange: (imageUrl: string) => void;
   disabled?: boolean;
+  error?: boolean;
 }
 
 const UploadInput: React.FC<UploadInputProps> = ({
@@ -20,27 +21,25 @@ const UploadInput: React.FC<UploadInputProps> = ({
   imageUrl,
   onImageChange,
   disabled = false,
+  error = false,
 }) => {
   const [loadingLocalImage, setLoadingLocalImage] = useState(false);
   const [localImageFile, setLocalImageFile] =
     useState<UploadFile<unknown> | null>(null);
 
+  const [showError, setShowError] = useState(false);
+
   const handleChange = (info: UploadChangeParam<UploadFile<unknown>>) => {
     const { type, status, size } = info?.file;
     const allowedFileTypes = ["image/jpeg", "image/png", "image/jpg"];
 
-    if (!allowedFileTypes.includes(type || "")) {
-      toast.error(
-        "Debe subir una imagen tipo JPG, PNG o JPEG con un maximo de 2 MB de peso"
-      );
+    if (!allowedFileTypes.includes(type || "") || (size || 0) > 2000000) {
+      setShowError(true);
       return;
     }
-    if ((size || 0) > 2000000) {
-      toast.error(
-        "Debe subir una imagen tipo JPG, PNG o JPEG con un maximo de 2 MB de peso"
-      );
-      return;
-    }
+
+    setShowError(false);
+    
     if (status === "uploading") {
       setLoadingLocalImage(true);
       return;
@@ -55,47 +54,56 @@ const UploadInput: React.FC<UploadInputProps> = ({
   };
 
   return (
-    <Upload
-      className={cn(
-        "w-full border rounded-xl font-century-gothic h-12 p-4 border-blue-500 relative cursor-pointer",
-        className
-      )}
-      name="localLogo"
-      listType="picture"
-      showUploadList={false}
-      onChange={handleChange}
-      disabled={disabled}
-    >
-      <div
-        className="flex items-center justify-between gap-2 absolute w-full h-full top-0 left-0 p-4 text-base"
-        title={localImageFile?.name || "Subir foto de perfil"}
+    <div className="relative flex flex-col items-start justify-center w-full">
+      <Upload
+        className={cn(
+          "w-full border rounded-xl font-century-gothic h-12 p-4 border-blue-500 relative cursor-pointer",
+          className,
+          (showError || error) && "border-red-500"
+        )}
+        name="localLogo"
+        listType="picture"
+        showUploadList={false}
+        onChange={handleChange}
+        disabled={disabled}
       >
-        {Boolean(imageUrl.trim()) && (
-          <Image
-            src={imageUrl}
-            alt=""
-            width={50}
-            height={50}
-            className="max-h-10"
-          />
-        )}
+        <div
+          className="flex items-center justify-between gap-2 absolute w-full h-full top-0 left-0 p-4 text-base"
+          title={localImageFile?.name || "Subir foto de perfil"}
+        >
+          {Boolean(imageUrl.trim()) && (
+            <Image
+              src={imageUrl}
+              alt=""
+              width={50}
+              height={50}
+              className="max-h-10"
+            />
+          )}
 
-        {loadingLocalImage ? (
-          <span className="block w-full">Cargando imagen...</span>
-        ) : (
-          <span className="block w-full truncate">
-            {(!localImageFile?.name && !imageUrl?.trim()) ||
-            (localImageFile?.name && !imageUrl?.trim())
-              ? "Subir foto de perfil"
-              : ""}
-            {localImageFile?.name && imageUrl.trim() && localImageFile?.name}
-            {!localImageFile?.name && imageUrl.trim() && "Imagen cargada"}
-          </span>
-        )}
+          {loadingLocalImage ? (
+            <span className="block w-full">Cargando imagen...</span>
+          ) : (
+            <span className="block w-full truncate">
+              {(!localImageFile?.name && !imageUrl?.trim()) ||
+              (localImageFile?.name && !imageUrl?.trim())
+                ? "Subir foto de perfil"
+                : ""}
+              {localImageFile?.name && imageUrl.trim() && localImageFile?.name}
+              {!localImageFile?.name && imageUrl.trim() && "Imagen cargada"}
+            </span>
+          )}
 
-        <FontAwesomeIcon icon={faArrowUpFromBracket} size="lg" />
-      </div>
-    </Upload>
+          <FontAwesomeIcon icon={faArrowUpFromBracket} size="lg" />
+        </div>
+      </Upload>
+      {(showError || error) && (
+        <span className="text-red-500 text-xs">
+          Debe subir una imagen tipo JPG, PNG o JPEG con un maximo de 2 MB de
+          peso
+        </span>
+      )}
+    </div>
   );
 };
 
