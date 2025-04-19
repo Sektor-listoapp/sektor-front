d62f39037ed59ae625c47c093066eb9b78e86dd5
Alexcab24 <sasuke246473@gmail.com>
2025-04-19 11:49:51 -0400

    customerForm added to constants.ts

--- src/components/my-account/constants.ts
+++ src/components/my-account/constants.ts
@@ -1,4 +1,4 @@
-import { OrganizationTypes } from "@/lib/sektor-api/__generated__/types";
+import { OrganizationTypes, UserGroups } from "@/lib/sektor-api/__generated__/types";
 import dynamic from "next/dynamic";
 
 const {
@@ -9,18 +9,13 @@ const {
   Supplier,
 } = OrganizationTypes;
 
+const { Customer } = UserGroups;
+
 export const USER_FORM = {
   [Supplier]: dynamic(() => import("@/components/my-account/supplier-form")),
-  [ExclusiveAgent]: dynamic(
-    () => import("@/components/my-account/exclusive-agent-form")
-  ),
-  [InsuranceBroker]: dynamic(
-    () => import("@/components/my-account/insurance-broker-form")
-  ),
-  [InsuranceCompany]: dynamic(
-    () => import("@/components/my-account/insurance-company-form")
-  ),
-  [BrokerageSociety]: dynamic(
-    () => import("@/components/my-account/brokerage-society-form")
-  ),
+  [ExclusiveAgent]: dynamic(() => import("@/components/my-account/exclusive-agent-form")),
+  [InsuranceBroker]: dynamic(() => import("@/components/my-account/insurance-broker-form")),
+  [InsuranceCompany]: dynamic(() => import("@/components/my-account/insurance-company-form")),
+  [BrokerageSociety]: dynamic(() => import("@/components/my-account/brokerage-society-form")),
+  [Customer]: dynamic(() => import("@/components/my-account/customer-form")),
 } as const;
