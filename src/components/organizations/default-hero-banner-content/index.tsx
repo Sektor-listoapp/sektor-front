0ff27674fb36ca6f1b623a63636e7e274d582a45
Alexcab24 <acabral2442@gmail.com>
2025-12-29 18:40:36 -0400

    Refactor DefaultHeroBannerContent layout and update image sources for improved responsiveness

--- src/components/organizations/default-hero-banner-content/index.tsx
+++ src/components/organizations/default-hero-banner-content/index.tsx
@@ -7,34 +7,25 @@ const DefaultHeroBannerContent = () => {
 
 
   return (
-    <div className="w-full gap-2 grid grid-cols-2 relative mx-auto sm:max-w-md md:max-w-full">
-      <header className="py-6 sm:py-16 md:py-24 z-10">
-        <h1 className="text-balance text-start text-lg sm:text-4xl lg:text-7xl w-full mb-6 sm:mb-8 md:mb-10">
+    <div className="w-full gap-2 grid grid-cols-2 relative mx-auto lg:grid-cols-12">
+      <header className="z-10 lg:col-span-5 py-10 md:py-20 lg:py-16">
+        <h1 className="text-start text-3xl sm:text-4xl md:text-5xl lg:text-7xl w-full mb-10">
           Nuestros Intermediarios
         </h1>
 
         <Link href="/clinic-list">
-          <Button className="text-xs sm:text-base px-4 sm:px-6 py-2 sm:py-3">
+          <Button className="hidden md:block">
             Ver listado de clínicas y seguros disponibles
           </Button>
         </Link>
-
-
       </header>
 
-      <div className="md:relative">
+      <div className="md:relative lg:col-span-7">
         <Image
-          className="hidden md:block w-full bottom-0 left-0 absolute md:max-w-96 lg:max-w-full"
-          src="/images/partners.webp"
+          className="w-3/4 bottom-0 right-0 absolute sm:max-w-96 md:w-[150%] md:max-w-lg lg:max-w-xl xl:max-w-3xl"
+          src="/images/suppliers.webp"
           width={1000}
           height={1000}
-          alt=""
-        />
-        <Image
-          className="md:hidden w-full max-w-[450px] bottom-0 left-[5%] absolute -z-0 sm:left-[15%] "
-          src="/images/partners-mobile.webp"
-          width={700}
-          height={700}
           alt=""
         />
       </div>
