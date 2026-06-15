9ef9397fc3cdd6ce9ab50b20e2811810ea2bbbfa
Alexcab24 <acabral2442@gmail.com>
2026-06-15 19:58:53 -0400

    Enhance Searchbar component by adding state management for search input and refactoring search logic. Implement active filter detection and streamline organization fetching based on search criteria. Update organization page to improve type handling for organization types.

--- src/utils/organizations/index.ts
+++ src/utils/organizations/index.ts
@@ -1,3 +1,4 @@
 export * from "./get-current-filters-from-query";
+export * from "./build-organization-query-variables";
 export * from "./get-location-options";
 export * from "./show-organization-details";
